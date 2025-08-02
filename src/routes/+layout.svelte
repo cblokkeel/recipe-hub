f42cd814cf6544021ae47ae1511d36e67aecef25
cblokkeel <cblokkeeltech@gmail.com>
2025-08-02 12:02:13 -0400

    chore: add convex

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { PUBLIC_CONVEX_URL } from '$env/static/public';
+	import { setupConvex } from 'convex-svelte';
 
 	let { children } = $props();
+
+	setupConvex(PUBLIC_CONVEX_URL);
 </script>
+
+{@render children()}
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
