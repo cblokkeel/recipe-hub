f0cb2c7c12392f5d7221284f29b9952f79390f3d
cblokkeel <cblokkeeltech@gmail.com>
2025-08-02 13:21:49 -0400

    chore: externalize convex client

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { PUBLIC_CONVEX_URL } from '$env/static/public';
 	import { setupConvexAuth } from '@mmailaender/convex-auth-svelte/sveltekit';
+	import { convexClient } from '$lib/convex';
 
 	let { children, data } = $props();
 
 	setupConvexAuth({
 		getServerState: () => data.authState,
-		convexUrl: PUBLIC_CONVEX_URL
+		client: convexClient
 	});
 </script>
 
