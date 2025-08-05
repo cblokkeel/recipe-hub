ecd939909e244e96f7f9dd7520c1dec5d8e83c46
cblokkeel <cblokkeeltech@gmail.com>
2025-08-05 10:59:16 -0400

    feat: filter recipes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,31 @@
 	import * as m from '$lib/paraglide/messages.js';
 	import type { PageProps } from './$types';
 	import Recipe from '$lib/recipes/Recipe.svelte';
+	import { Search } from '@lucide/svelte';
 
 	let { data }: PageProps = $props();
+
+	let filter = $state('');
+
+	const filteredRecipes = $derived(
+		data.recipes.filter((recipe) => recipe.name.toLowerCase().includes(filter.toLowerCase()))
+	);
 </script>
 
-<a class="btn btn-ghost" href="/create-recipe/manual">{m['add_recipe.title']()}</a>
+<div class="flex flex-col gap-4">
+	<div class=" flex items-center justify-between">
+		<h1 class="text-lg font-bold">{m['recipes.title']()}</h1>
+		<a class="btn" href="/create-recipe/manual">{m['recipes.add_recipe']()}</a>
+	</div>
 
-{#each data.recipes as recipe, index (index)}
-	<Recipe {recipe} />
-{/each}
+	<label class="input flex w-full items-center">
+		<Search size="16" />
+		<input type="text" required placeholder={m['recipes.search_recipes']()} bind:value={filter} />
+	</label>
+
+	<div>
+		{#each filteredRecipes as recipe, index (index)}
+			<Recipe {recipe} />
+		{/each}
+	</div>
+</div>
