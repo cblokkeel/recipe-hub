36ff24682f5cd8c4ca561004110e32b6cf4cfac9
cblokkeel <cblokkeeltech@gmail.com>
2025-08-05 09:44:07 -0400

    chore: increase jwt expiration to 30 days

--- src/convex/auth.ts
+++ src/convex/auth.ts
@@ -2,5 +2,8 @@ import { convexAuth } from '@convex-dev/auth/server';
 import Google from '@auth/core/providers/google';
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-	providers: [Google]
+	providers: [Google],
+	jwt: {
+		durationMs: 60 * 60 * 24 * 30 * 1000
+	}
 });
